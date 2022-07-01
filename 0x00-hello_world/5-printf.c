839af50421e08574fa3d1f16c5ce49c308873580
maunainiko <mkodoosi@gmail.com>
2022-06-30 18:01:11 -0700

    size

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main = Entry Point
+*Description: print program using printf
+*use main function
+*Result: Alwyas 0 (Success)
+*/
+int main (void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}

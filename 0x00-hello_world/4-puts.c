4d70eb9cf6c5102c8c650654a1dde378c7ba852f
maunainiko <mkodoosi@gmail.com>
2022-06-30 18:48:54 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- *main - Print words using put
- *
- *Description: use the main function
- *to print ""Programming is like building a multilingual puzzle"
- *Return: Always 0 (Success)
- */
+*main - Print words using put
+*
+*Description: use the main function
+*to print ""Programming is like building a multilingual puzzle"
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

0bcfbac8421b3e772e64a2d34a57735f68e36ade
maunainiko <mkodoosi@gmail.com>
2022-06-30 18:22:58 -0700

    indent

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-*main = Print words using put
-*
-*Description: use the main function
-*to print ""Programming is like building a multilingual puzzle"
-*Return: Always 0 (Success)
-*/
+ *main - Print words using put
+ *
+ *Description: use the main function
+ *to print ""Programming is like building a multilingual puzzle"
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
-*main = Entry Point
-*Description: print program using printf
-*use main function
-*Result: Alwyas 0 (Success)
-*/
+ *main = Entry Point
+ *Description: print program using printf
+ *use main function
+ *Result: Alwyas 0(Success)
+ */
+
 int main (void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n");

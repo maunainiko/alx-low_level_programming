4ade12f07a9c14be0d67c9ae6e5bc55e6a26619f
maunainiko <mkodoosi@gmail.com>
2022-06-30 18:53:24 -0700

    spaces

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- *main = Entry Point
- *
- *Description: use main function
- *Result: Always 0(Success)
- */
+*main = Entry Point
+*
+*Description: use main function
+*this program prints a string
+*Result: Always 0(Success)
+*/
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }

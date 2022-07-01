67787dfcde1aa6b3177d53a4ee306a46602da484
maunainiko <mkodoosi@gmail.com>
2022-06-30 18:39:08 -0700

    siz

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  *main = Entry Point
- *Description: print program using printf
- *use main function
- *Result: Alwyas 0(Success)
+ *
+ *Description: use main function
+ *Result: Always 0(Success)
  */
 
-int main (void)
+int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n");
   return (0);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,7 @@
 *main - Entry level
 *
 *Decription: using the main function
+*this program prints the size of the string
 *return 0
 */
 int main(void)
@@ -17,7 +18,7 @@ int main(void)
   printf("Size of a char: %ld byte(s)\n", sizeof(c));
   printf("Size of an int: %ld byte(s)\n", sizeof(i));
   printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-  printf("Size of a long long: %ld byte(s)\n", sizeof(lli));
+  printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
   printf("Size of a float: %ld byte(s)\n", sizeof(f));
   return (0);
 }

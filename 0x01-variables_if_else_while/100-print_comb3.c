207bd39c80360afde8887cb0efc7249740d72755
maunainiko <mkodoosi@gmail.com>
2022-07-04 16:00:28 -0700

    m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,11 @@ int digit1, digit2;
 
 for (digit1 = 0; digit1 < 10; digit1++)
 {
-for (digit2 = !digit1 && digit2 < digit1; digit2++)
+for (digit1 < 10; digit2 != digit1 && digit2 < digit1; digit2++)
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
-if (digit1 == 8 && digit2 == 9)
-continue;
+if (digit1 + digit2 != 17)
+{
 putchar(',');
 putchar(' ');
 }

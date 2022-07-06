b0e0252d4f963457ee12360155d77e7bebf58da0
maunainiko <mkodoosi@gmail.com>
2022-07-06 06:20:41 -0700

    Create a putchar program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A program that prints _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+
+}

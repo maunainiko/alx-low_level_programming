23e07a3f03a0b4d5e16cc0b1b50c68eb90572e89
maunainiko <mkodoosi@gmail.com>
2022-07-06 11:42:08 -0700

    nested

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print alphabet in lower case
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+
+
+	_putchar('\n');
+
+
+
+}
+
